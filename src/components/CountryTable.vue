3ebc316f734e5964ecd12afc434efb6aabdbe20f
shambhu047 <mr.shambhu04@gmail.com>
2017-03-06 12:19:10 +0530

    Add files via upload

--- src/components/CountryTable.vue
+++ src/components/CountryTable.vue
@@ -86,43 +86,6 @@ import { mapState } from 'vuex'
 
 export default {
     data: () => ({
-    nutrition: [
-      {
-        dessert: 'Frozen yogurt',
-        type: 'ice_cream',
-        calories: '159',
-        fat: '6.0',
-        comment: 'Icy'
-      },
-      {
-        dessert: 'Ice cream sandwich',
-        type: 'ice_cream',
-        calories: '237',
-        fat: '9.0',
-        comment: 'Super Tasty'
-      },
-      {
-        dessert: 'Eclair',
-        type: 'pastry',
-        calories: '262',
-        fat: '16.0',
-        comment: ''
-      },
-      {
-        dessert: 'Cupcake',
-        type: 'pastry',
-        calories: '305',
-        fat: '3.7',
-        comment: ''
-      },
-      {
-        dessert: 'Gingerbread',
-        type: 'other',
-        calories: '356',
-        fat: '16.0',
-        comment: ''
-      }
-    ],
     currentPage : 1,
     pageSize: 15,
     searchAttribute: '',
